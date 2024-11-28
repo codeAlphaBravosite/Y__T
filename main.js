e34020098049b439e9428f64172759f3a2f26b29
codeAlphaBravosite <help.factchannel@gmail.com>
2024-11-28 19:35:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,27 +3,27 @@ const tools = [
     {
         title: 'Similar Channel Viewer',
         description: 'Find similar YouTube channels and discover new content creators in your niche.',
-        url: 'https://coli.github.io/simich/'
+        url: 'https://codealphabravosite.github.io/simich/'
     },
     {
         title: 'YT Video Viewer',
         description: 'Find best youtube video from search and channels',
-        url: 'https://coli.github.io/srch-vd/'
+        url: 'https://codealphabravosite.github.io/srch-vd/'
     },
     {
         title: 'Publish Date Checker',
         description: 'Check the exact publish date and time of any YouTube video.',
-        url: 'https://coli.github.io/pub_d/'
+        url: 'https://codealphabravosite.github.io/pub_d/'
     },
     {
         title: 'YT Comments Extractor',
         description: 'Extract all youtube comments with ease and download as csv',
-        url: 'https://coli.github.io/cmnt/'
+        url: 'https://codealphabravosite.github.io/cmnt/'
     },
     {
-        title: 'Online Notepad',
-        description: 'Write down quick notes and print a simple text document with Online Notepad editor.',
-        url: 'https://coli.github.io/Noteepadd/'
+        title: 'Word Counter',
+        description: 'Copy and paste your text into the online editor to count its words and characters.',
+        url: 'https://codealphabravosite.github.io/Noteepadd/'
     }
 ];
 
